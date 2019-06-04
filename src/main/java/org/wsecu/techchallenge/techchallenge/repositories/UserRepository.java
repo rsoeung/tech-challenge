c7f959f751c2ced557050a0dc1bf76b138d77915
Reasmei Soeung <reasmei@hotmail.com>
2019-06-03 22:02:24 -0700

    Test

--- src/main/java/org/wsecu/techchallenge/techchallenge/repositories/UserRepository.java
+++ src/main/java/org/wsecu/techchallenge/techchallenge/repositories/UserRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.wsecu.techchallenge.techchallenge.domain.User;
 
 public interface UserRepository extends JpaRepository<User, String> {
-    User findByUsername(String username);
+    User findByUsername(String username);//
 }
